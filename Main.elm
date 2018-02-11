acc6f70723476671f8cd7888d0ab2f9c119e3ba2
Jakub <nawias@gmail.com>
2018-02-11 07:37:48 +0100

    abstract list of squares in main

--- Main.elm
+++ Main.elm
@@ -8,4 +8,17 @@ import Html exposing (..)
 
 main : Html.Html msg
 main =
-    toHtml (collage 100 100 [ filled (rgb 100 100 100) (rect 10 10) ])
+    let
+        squares =
+            List.map (\el -> drawSqr (toFloat el * 20) 10 (el * 40)) (List.range 1 10)
+    in
+    toHtml (collage 1000 500 squares)
+
+
+drawSqr : Float -> Float -> Int -> Form
+drawSqr movX movY rCol =
+    move ( movX, movY )
+        (filled
+            (rgb rCol 100 100)
+            (rect 10 10)
+        )
